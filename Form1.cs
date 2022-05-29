027fb7408e123340d2cbd5f2eec8505f7ef8e081
zgh323 <zhugaohao@sina.com>
2022-05-30 00:21:18 +0800

    初始状态

---
+++ Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NHPC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ck_Click(object sender, EventArgs e)
+        {
+            int index = this.dataGridView1.Rows.Add();
+
+        }
+    }
+}
